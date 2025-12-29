b28ae8d3ece2dfac4d85b94fc4fb9e2d75d17557
Waheed-Ashraf <57561916+Waheed-Ashraf@users.noreply.github.com>
2025-12-29 17:44:25 +0200

    Deploy Flutter portfolio to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3313722513"
+    serviceWorkerVersion: "2084213272"
   }
 });
