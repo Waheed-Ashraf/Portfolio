5227b0b3d87e232e1c9acc10cfc8adb4b484c48f
Waheed-Ashraf <57561916+Waheed-Ashraf@users.noreply.github.com>
2026-07-05 21:08:07 +0300

    Deploy Flutter web to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2396966439" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2859630182" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
